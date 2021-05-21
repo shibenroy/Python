d0212063e1d22386817d4d065ee5069ab1d4b8e4
Shiben-Roy <pratima.roy09@gmail.com>
2021-05-21 21:44:12 +0530

    Learning Matrix In Python
    Matrix is python

---
+++ Files/MatrixinPython.py
@@ -0,0 +1,11 @@
+from numpy import *
+
+arr = array([
+                  [1,5,6]
+                  [3,9,7]
+            ])
+print(arr.ndim)
+            
+             
+
+                
--- Files/Pattern.py
+++ Files/Pattern.py
@@ -1,9 +1,9 @@
-d = 1
+d = 5
 
 
 for i in range(5):
     # print(1)
-    for j in range(i+1):
+    for j in range(i-1):
         print("*",end=" ")
     print()
 
