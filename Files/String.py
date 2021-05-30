78a6f6d0b55c85b00b0cf7e257dba447f9c875d5
Shiben-Roy <pratima.roy09@gmail.com>
2021-05-30 21:42:30 +0530

    Update String.py

--- Files/String.py
+++ Files/String.py
@@ -55,7 +55,7 @@ while(True):
 
 
     else:
-        speak("Incorrect input provied. Please select options between 1 to 7 only.")
+        speak("Incorrect input provided. Please select options between 1 to 7 only.")
         print("incorrect input")
 
     myask2 = ""
