20b2419657d4d4e65555fce4575c0c8f0408bd88
Shiben-Roy <pratima.roy09@gmail.com>
2021-06-11 21:02:21 +0530

    Maths in python :0

--- Files/math/Maths.py
+++ Files/math/Maths.py
@@ -1,4 +1,9 @@
+'''
+Creater By: Shiben Roy
+
+'''
 print('Loading Converter')
+
 # Metre to Centimetre
 # metre to kilometre
 # Area Finder
@@ -34,33 +39,57 @@ def areafind1():
     intbreadth = int(breadth1)
     lbtotal = intlen * intbreadth
     print(lbtotal)
-    
-factorNum = ''
 
+def perimeterfind1():
+    print('please inter length and breadth')
+    length2 = input('pleas inter length')
+    breadth2 = input('please input breadth')
+    len2 = int(length2)
+    beat2 = int(breadth2)
+    lb2 = len2 + beat2
+    lbx = lb2  + lb2
+    print(lbx)
+    
+
+while True:
    
-print('What do you want')
-print('Metre to Centimetre (mtc) ')
-print('metre to kilometre (mtk) ')
-print('Factor')
-print('')
+    print('What do you want')
+    print('Metre to Centimetre (mtc) ')
+    print('metre to kilometre (mtk) ')
+    print('Factor')
+    print('Find Area(AF)')
+    print("Find Perimeter(FP)")
 
-factorNum = input('Pls input choice')
+    print('')
+
+    factorNum = input('Pls input choice >  ')
+
+        
+    myask1 = ''
+
+
+    if factorNum == 'mtc':
+        mtc()
+    elif factorNum == 'mtk':
+        mtk()
+    elif factorNum == 'AF':
+        areafind1() 
+    elif factorNum == 'FP':
+        perimeterfind1()       
+    else:
+        print('Wrong Input')
+
+
+    print('input q to quit and c to countinue ')    
+
+    while(countinue != 'q' and countinue != 'c' ):
+        countinue = input('hi')
+        if countinue == 'q':
+            exit()
+        elif countinue =='c':
+            countinue    
 
     
-
-
-
-if factorNum == 'mtc':
-    mtc()
-
-elif factorNum == 'mtk':
-
-    mtk()
-elif factorNum == 'areafind1':
-    areafind1()    
-else:
-    print('Wrong Input')    
-
 
 
 
